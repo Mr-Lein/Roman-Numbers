aa10bf4ea61e8dc25f7f7297051fb81626efea68
Mr-Lein <151097135+Mr-Lein@users.noreply.github.com>
2024-09-18 15:16:04 -0600

    Update Romans.java

--- Romans.java
+++ Romans.java
@@ -12,23 +12,12 @@ public class Romans
 	{
 		m = new TreeMap<String, Integer>();
 		
-		/*
-			Symbol	I	V	X		L		C		D		M
-			Value		1	5	10		50		100	500	1000
-		*/
-		
-		m.put( "I", 1 );
-		m.put( "V", 5 );
-		m.put( "X", 10 );
-		m.put( "L", 50 );
-		m.put( "C", 100 );
-		m.put( "D", 500 );
-		m.put( "M", 1000 );					
+		//complete your map				
 	}
 	
 	public int getNumber( String s )
 	{
-		return m.get( s );
+		//complete this method
 	}
 
 }
